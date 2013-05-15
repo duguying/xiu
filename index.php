a0e528bc06badbbc3a4d8896fedbddb992654984
rex lee <duguying2008@gmail.com>
2013-05-15 23:06:44 +0800

    fix entrance file

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 define('APP_DEBUG',TRUE);
-define('APP_NAME','main');
 define('APP_PATH','./main/');
-define('THINK_PATH','./ThinkPHP/');
 define('ENGINE_NAME','cluster');
 require '../ThinkPHP/ThinkPHP.php';
