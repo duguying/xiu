906cc0a09e3c6980a7baf30230dc30286843a213
rex <duguying2008@gmail.com>
2013-10-20 17:56:14 +0800

    clean

--- Public/FF/js/main.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-;$(document).ready(function(e) {
-    //console.log("a test");
-	//console.log($(".top_three>ul").length);
-	var uls=$(".top_three>ul");
-	for(var i=0;i<uls.length;i++){
-		$(uls[i])//.html(i);
-		}
-});
