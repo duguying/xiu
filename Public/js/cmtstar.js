0b557d4332b5c37d90b534d54c099ecdd6eae180
rex <duguying2008@gmail.com>
2013-10-27 13:51:28 +0800

    tpl refactor

--- Public/js/cmtstar.js
+++ Public/js/cmtstar.js
@@ -138,11 +138,11 @@ function tpc_detail(id,title){
 	}else{
 		var tpc_name='';
 	}
-	var hstr='<div id="tpc_frame"><span class="tpc_title">主题：'+tpc_name+'<div tbtn>X</div></span><iframe id="page" allowtransparency="false" src="'+U('tpcdetail/tpc/'+tpc_id)+'"  frameborder="0" marginwidth="0" marginheight="0" scrolling="auto" ></iframe></div>'
+	var hstr='<div id="tpc_frame"><span class="tpc_title">主题：'+tpc_name+'<div id="tbtn">X</div></span><iframe id="page" allowtransparency="false" src="'+U('tpcdetail/tpc/'+tpc_id)+'"  frameborder="0" marginwidth="0" marginheight="0" scrolling="auto" ></iframe></div>'
 	$("#tpc_frame").remove();
 	$("body").append(hstr);
 	$("#tpc_frame").draggable();
-	$("div[tbtn]").click(function(e) {
+	$("div#tbtn").click(function(e) {
         $("#tpc_frame").remove();
     });
 };
